472b3f30632ad873d931def83d4b9e0315473345
Stephen Muss <stephenmuss@gmail.com>
2012-07-04 15:48:26 +1000

    Added management command to call feedback service

---
+++ ios_notifications/management/commands/call_feedback_service.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand, CommandError
+from ios_notifications.models import FeedbackService
+from optparse import make_option
+
+# TODO: argparse for Python 2.7
+
+
+class Command(BaseCommand):
+    help = 'Calls the Apple Feedback Service to determine which devices are no longer active and deactivates them in the database.'
+
+    option_list = BaseCommand.option_list + (
+        make_option('--feedback-service',
+            help='The id of the Feedback Service to call',
+            dest='service',
+            default=None),)
+
+    def handle(self, *args, **options):
+        if options['service'] is None:
+            raise CommandError('The --feedback-service option is required')
+        try:
+            service_id = int(options['service'])
+        except ValueError:
+            raise CommandError('The --feedback-service option should pass an id in integer format as its value')
+        try:
+            service = FeedbackService.objects.get(pk=service_id)
+        except FeedbackService.DoesNotExist:
+            raise CommandError('FeedbackService with id %d does not exist' % service_id)
+
+        num_deactivated = service.call()
+        output = '%d device%s were deactivated.\n' % (num_deactivated, '' if num_deactivated == 1 else 's')
+        self.stdout.write(output)
