ea2df42434a39f5fc008c83b14459ba4958c4a12
Stephen Muss <stephenmuss@gmail.com>
2012-07-10 08:57:27 +1000

    Updated download url in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description='Django iOS Notifications makes it easy to send push notifications to iOS devices',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     url='https://github.com/stephenmuss/django-ios-notifications',
-    download_url='https://github.com/stephenmuss/django-ios-notifications/zipball/v0.1',
+    download_url='https://github.com/stephenmuss/django-ios-notifications/zipball/v0.1.1',
     license='BSD License',
     packages=find_packages(),
     include_package_data=True,
