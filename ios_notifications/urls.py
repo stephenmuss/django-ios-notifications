869f797157ce28cce810f080229ea93baf625c69
Valentín Moreno <valenmope@gmail.com>
2016-10-21 17:05:46 +0200

    fix on urls

--- ios_notifications/urls.py
+++ ios_notifications/urls.py
@@ -3,7 +3,7 @@
 from django.conf.urls import url
 from .api import routes
 
-urlpatterns = ['',
+urlpatterns = [
     url(r'^device/$', routes.device, name='ios-notifications-device-create'),
     url(r'^device/(?P<token>\w+)/(?P<service__id>\d+)/$', routes.device, name='ios-notifications-device'),
 ]
